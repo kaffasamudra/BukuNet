ddb1312858767e07cfb992bb32883624efb17e34
kaffasamudra <kaffas87@gmail.com>
2024-12-14 12:39:15 +0700

    commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,3 +6,8 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['dashboard'] = 'user/Dashboard/index';
+$route['loginuser'] = 'user/Users/index';
+$route['userlogin'] = 'user/Users/login';
+
+$route['registrasi'] = 'user/Users/registrasi';
+$route['register'] = 'user/Users/register';
