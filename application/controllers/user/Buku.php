b518c6b4544bfb9253de4c6b2833345ff8b3f294
kaffasamudra <kaffas87@gmail.com>
2024-12-28 16:51:15 +0700

    coommiitt

--- application/controllers/user/Buku.php
+++ application/controllers/user/Buku.php
@@ -7,11 +7,13 @@ class Buku extends CI_controller
 	{
 		parent::__construct();
 		$this->load->model('M_buku');
+		$this->load->model('M_users');
 	}
 
 	public function index()
 	{
 		$data['buku']=$this->M_buku->get_buku();
+		$data['users'] = $this->M_users->get_users();
 		$this->load->view('user/buku', $data);
 	}
 }
