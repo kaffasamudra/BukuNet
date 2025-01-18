08934d57ef29d756e360f81decb89cfa4309458e
kaffasamudra <kaffas87@gmail.com>
2025-01-18 08:21:52 +0700

    commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,25 +5,29 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['bukunet'] = 'user/Dashboard/index';
 $route['loginuser'] = 'user/Users/index';
 $route['userlogin'] = 'user/Users/login';
-
-$route['registrasi'] = 'user/Users/registrasi';
-$route['register'] = 'user/Users/register';
 
 $route['loginadmin'] = 'admin/Admin/index';
 $route['adminlogin'] = 'admin/Admin/login';
 
-$route['dashboard'] = 'admin/Dashboard/index';
+$route['registrasi'] = 'user/Users/registrasi';
+$route['register'] = 'user/Users/register';
+
 $route['registrasii'] = 'admin/Admin/registrasi';
 $route['registerr'] = 'admin/Admin/register';
 
-$route['buku'] = 'user/Buku/index';
-$route['peminjaman'] = 'user/peminjaman/index';
+$route['admin'] = 'admin/Dashboard/index';
+$route['petugas'] = 'admin/Dashboard/petugas';
 
-$route['peminjaman/form/(.+)'] = 'peminjaman/form/$1';
+$route['bukunet'] = 'user/Dashboard/index';
+$route['buku'] = 'user/Buku/index';
+
+$route['peminjaman'] = 'user/peminjaman/index';
 $route['peminjaman/simpan'] = 'user/peminjaman/simpan';
 
+$route['peminjaman/form/(.+)'] = 'peminjaman/form/$1';
 $route['editform/(.+)'] = 'admin/data_peminjaman/edit/$1';
 $route['datapeminjaman'] = 'admin/data_peminjaman/index';
+$route['cekjumlah/(.+)'] = 'user/peminjaman/cek_jumlah/$1';
+
