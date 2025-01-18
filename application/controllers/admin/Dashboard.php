0f596dd8e83cc62cc0a9242a7f039be71bee1424
kaffasamudra <kaffas87@gmail.com>
2025-01-18 08:22:06 +0700

    comimti

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -5,10 +5,23 @@ class Dashboard extends CI_Controller
     public function __construct() {
         parent::__construct();
         $this->load->model('M_admin');
+        $this->load->model('M_buku');
+        $this->load->model('M_peminjaman');
     }
 
 	public function index() {
-		$data['admin'] = $this->M_admin->get_users();
-		$this->load->view('admin/dashboard', $data);
-	}
+        $data['admin'] = $this->M_admin->get_users();
+        $data['count_buku'] = $this->M_buku->count_buku();
+        $data['count_peminjaman'] = $this->M_peminjaman->count_peminjaman();
+
+        $this->load->view('admin/dashboard', $data);
+    }
+
+    public function petugas() {
+        $data['admin'] = $this->M_admin->get_users();
+        $data['count_buku'] = $this->M_buku->count_buku();
+        $data['count_peminjaman'] = $this->M_peminjaman->count_peminjaman();
+
+        $this->load->view('admin/petugas/dashboard', $data);
+    }
 }
