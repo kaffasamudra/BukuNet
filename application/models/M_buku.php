72f5c1023ab80b7d801f802e2900e94ab96c5cf1
kaffasamudra <kaffas87@gmail.com>
2025-01-28 08:23:54 +0700

    comiiiiitt

--- application/models/M_buku.php
+++ application/models/M_buku.php
@@ -10,4 +10,22 @@ class M_buku extends CI_Model
     public function get_buku_id($id) {
         return $this->db->get_where('buku', ['id' => $id])->row();
     }
+
+    public function count_buku() {
+        return $this->db->count_all('buku');
+    }
+
+    public function cek_jumlah_buku($id_buku) {
+        $this->db->select('jumlah');
+        $this->db->from('buku');
+        $this->db->where('id', $id_buku);
+        $query = $this->db->get();
+        $result = $query->row();
+
+        if (!$result) {
+            return false; // Buku tidak ditemukan
+        }
+
+        return ($result->jumlah > 0); // True jika jumlah tersedia, False jika habis
+    }
 }
