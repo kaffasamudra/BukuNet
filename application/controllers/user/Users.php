0bf1c3da473c66aed695a352c5c5271954acefaa
kaffasamudra <kaffas87@gmail.com>
2025-01-28 08:23:43 +0700

    cooooomit

--- application/controllers/user/Users.php
+++ application/controllers/user/Users.php
@@ -26,9 +26,9 @@ class Users extends CI_Controller
 			$email = $this->input->post('email');
 			$password = $this->input->post('password');
 
-			$user = $this->user->get_user($email, $password);
+			$user = $this->user->get_user($email);
 
-			if ($user->email == $email && $user->password) {
+			 if ($user && password_verify($password, $user->password)) {
 				$this->session->set_userdata('nama', $user->nama);
 				$this->session->set_userdata('email', $user->email);
 				$this->session->set_userdata('alamat', $user->alamat);
@@ -48,32 +48,33 @@ class Users extends CI_Controller
 	}
 
 	public function register() {
-        $this->load->library('form_validation');
+	    $this->load->library('form_validation');
 
-        $this->form_validation->set_rules('nama', 'Nama', 'required');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
-        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-        $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
-        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+	    $this->form_validation->set_rules('nama', 'Nama', 'required');
+	    $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
+	    $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+	    $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
+	    $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
 
-        if ($this->form_validation->run() == FALSE) {
-            $this->load->view('user/register');
-        } else {
-            $data = [
-                'nama'     => $this->input->post('nama'),
-                'email'    => $this->input->post('email'),
-                'password' => $this->input->post('password'),
-            ];
+	    if ($this->form_validation->run() == FALSE) {
+	        $this->load->view('user/register');
+	    } else {
+	        $data = [
+	            'nama'     => $this->input->post('nama'),
+	            'email'    => $this->input->post('email'),
+	            'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT), // Hash password
+	        ];
 
-            if ($this->user->insert_user($data)) {
-                $this->session->set_flashdata('success', 'Registration successful!');
-                redirect('loginuser');
-            } else {
-                $this->session->set_flashdata('error', 'Registration failed, try again!');
-                redirect('registrasi');
-            }
-        }
-    }
+	        if ($this->user->insert_user($data)) {
+	            $this->session->set_flashdata('success', 'Registration successful!');
+	            redirect('loginuser');
+	        } else {
+	            $this->session->set_flashdata('error', 'Registration failed, try again!');
+	            redirect('registrasi');
+	        }
+	    }
+	}
+
 
     public function logout() {
 	    $this->session->sess_destroy();
